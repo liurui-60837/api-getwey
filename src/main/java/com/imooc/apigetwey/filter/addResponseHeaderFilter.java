aa6404cab323a596df91e74a433f4ac7e2256769
liurui-60837 <liurui001@sinosoft.com.cn>
2021-07-26 17:41:02 +0800

    zuul实现前置过滤的校验和后置设置信息

---
+++ src/main/java/com/imooc/apigetwey/filter/addResponseHeaderFilter.java
@@ -0,0 +1,40 @@
+package com.imooc.apigetwey.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.omg.CORBA.TRANSACTION_MODE;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.*;
+
+@Component
+public class addResponseHeaderFilter extends ZuulFilter {
+
+    @Override
+    public String filterType() {
+        return POST_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return SEND_RESPONSE_FILTER_ORDER-1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext requestContext = RequestContext.getCurrentContext();
+        HttpServletResponse response = requestContext.getResponse();
+        response.setHeader("x-for","123");
+
+        return null;
+    }
+}
